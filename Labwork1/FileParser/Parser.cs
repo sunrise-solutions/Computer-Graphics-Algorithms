ee68049dc4eb7a41fd8fe3a89661b0ad81df0ca2
Vita <vita.petrovskaya@gmail.com>
2019-09-03 12:45:42 +0300

    Add parser

---
+++ Labwork1/FileParser/Parser.cs
@@ -0,0 +1,35 @@
+﻿using Labwork1.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labwork1.FileParser
+{
+    public static class Parser
+    {
+        public static GraphObject ParseFile(string filePath)
+        {
+            GraphObject graphObject = new GraphObject();
+
+            using (FileStream fs = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (BufferedStream bs = new BufferedStream(fs))
+                {
+                    using (StreamReader sr = new StreamReader(bs))
+                    {
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
