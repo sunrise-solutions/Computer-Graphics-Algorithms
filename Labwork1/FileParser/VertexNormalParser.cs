d94a9c80f10032492f9ea7ef082da78a94c7ee2b
npovalyaeva <iriejamaica@outlook.com>
2019-09-27 13:51:43 +0300

    Add file elements parsers

---
+++ Labwork1/FileParser/VertexNormalParser.cs
@@ -0,0 +1,21 @@
+﻿using Labwork1.Models;
+using System;
+
+namespace Labwork1.FileParser
+{
+    public static class VertexNormalParser
+    {
+        public static VertexNormal Parse(string line)
+        {
+            string[] parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            VertexNormal vertexNormal = new VertexNormal();
+
+            vertexNormal.X = parts[0].ParseInvariantFloat();
+            vertexNormal.Y = parts[1].ParseInvariantFloat();
+            vertexNormal.Z = parts[2].ParseInvariantFloat();
+
+            return vertexNormal;
+        }
+    }
+}
---
+++ Labwork1/FileParser/VertexParser.cs
@@ -0,0 +1,22 @@
+﻿using Labwork1.Models;
+using System;
+
+namespace Labwork1.FileParser
+{
+    public static class VertexParser
+    {
+        public static Vertex Parse(string line)
+        {
+            string[] parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Vertex vertex = new Vertex();
+
+            vertex.X = parts[0].ParseInvariantFloat();
+            vertex.Y = parts[1].ParseInvariantFloat();
+            vertex.Z = parts[2].ParseInvariantFloat();
+            vertex.Z = (parts.Length > 3) ? parts[2].ParseInvariantFloat() : 1;
+
+            return vertex;
+        }
+    }
+}
